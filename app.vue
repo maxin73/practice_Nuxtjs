e09de62a9c2f45910ef07b52c8051853228865ab
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-11-30 11:33:42 -0800

    update routing

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    Hello Nuxt3
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
