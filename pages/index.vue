d6d016c3a29671b5af3491abe9c1874d68289525
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-12-06 01:22:01 -0800

    update composables

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
+<script>
+const { sayHello } = useUtils();
+sayHello();
+// const sayHello = () => console.log("Hello");
+// sayHello();
+</script>
+
 <template>
-  <div class="Main">
+  <div class="container py-3 mx-auto Main">
     <!-- <Alert />
     <Profile /> -->
-    Main Page
-    <template>
-      <img src="~/assets/apple.png" alt="" />
-    </template>
-
+    <h1 class="text-teal-500">Main Page</h1>
+    <!-- <img src="~/assets/apple.png" alt="" /> -->
   </div>
 </template>
---
+++ pages/profile.vue
@@ -0,0 +1,13 @@
+<script>
+const sayHello = () => console.log("Goodbye");
+sayHello();
+</script>
+
+<template>
+  <div class="container py-3 mx-auto Profile">
+    <!-- <Alert />
+    <Profile /> -->
+    <h1 class="text-red-500">Profile Page</h1>
+    <!-- <img src="~/assets/apple.png" alt="" /> -->
+  </div>
+</template>
